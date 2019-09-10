118619a1521d24e9fa65110e8b6ccf94a17b320c
GproSoftware828 <JetSendProductions@Gmail.com>
2019-09-10 15:37:15 +1000

    Added devise scaffolding for React components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
+//import axios from 'axios-on-rails';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+    this.updateCurrentUser = this.updateCurrentUser.bind(this);
+  }
+  componentDidMount() {
+    let that = this;
+    axios
+      .get('/users/check_for_user', {})
+      .then(function(response) {
+        if (response.data.email) {
+          that.setState({
+            currentUser: response.data.email
+          });
+        } else {
+          that.setState({
+            currentUser: null
+          });
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  updateCurrentUser(email) {
+    this.setState({
+      currentUser: email
+    });
+  }
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <Header updateCurrentUser={this.updateCurrentUser} />
+      </div>
+    );
   }
 }
 export default App;
